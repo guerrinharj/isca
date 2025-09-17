0a4662cf2bb07e7e56f49c693928758d1b79682c
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2025-09-17 19:43:33 -0300

    front PUT

--- app/[locale]/cardapio/[id]/edit/page.tsx
+++ app/[locale]/cardapio/[id]/edit/page.tsx
@@ -17,6 +17,7 @@ type Prato = {
     is_alcoolico: boolean
     is_soft: boolean
     is_outro: boolean
+    is_vinho: boolean
 }
 
 export default function EditPratoPage({
@@ -215,6 +216,15 @@ export default function EditPratoPage({
                             checked={form.is_outro}
                             onChange={onInputChange}
                         /> Outro
+                    </label>
+                    <label className="block">
+                        <input
+                            type="checkbox"
+                            className="mx-2 align-middle"
+                            name="is_vinho"
+                            checked={form.is_vinho}
+                            onChange={onInputChange}
+                        /> Vinho
                     </label>
                 </div>
 
